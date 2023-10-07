c776224f09a088f4393e44d9fd10e16e54da02e0
Shirin <shirin.mohajer@gmail.com>
2023-10-07 09:30:40 -0400

    finished lab

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -1,7 +1,13 @@
 const Plant = require('../models/plant');
+module.exports = {
+    getAllPlants,
+    getOnePlant,
+    createPlant,
+    updatePlant,
+    deletePlant
+};
 
-
-const getAllPlants = async (req, res) => {
+async function getAllPlants(req, res) {
     try {
         const PORT = process.env.PORT || 3001;
 
@@ -13,4 +19,55 @@ const getAllPlants = async (req, res) => {
     }
 };
 
-module.exports = { getAllPlants };
+async function getOnePlant(req, res) {
+    try {
+        const id = req.params.id;
+        const plant = await Plant.findById(id);
+        return plant ? res.json(plant) : res.status(404).send(`plannt with spesific ID does not exist `);
+    } catch (error) {
+
+        return res.status(500).send(error.message);
+    }
+}
+
+//post
+async function createPlant(req, res) {
+    try {
+        const plant = await new Plant(req.body);
+        await plant.save();
+        return res.status(201).json({ plant });
+    } catch (error) {
+
+        return res.status(500).send(error.message);
+    }
+}
+
+//put
+async function updatePlant(req, res) {
+    try {
+        const id = req.params.id;
+        const plant = await Plant.findByIdAndUpdate(id, req.body, { new: true });
+        if (plant) {
+            return res.status(200).json(plant);
+        }
+        throw new Error("Plant not found");
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
+}
+
+async function deletePlant(req, res) {
+    try {
+        const id = req.params.id;
+        let plant = await Plant.findByIdAndDelete(id);
+        if (plant) {
+            return res.status(200).json(plant);
+        }
+
+    } catch (error) {
+
+        return res.status(500).send(error.message);
+    }
+}
+
+
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const db = require('./db');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const plantController = require('./controllers/plantController');
 
-// require() imports and middleware here ^ ///////
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.use() middleware here ^ ///////////////////
+app.use(logger('dev'));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-app.get('/plants', plantController.getAllPlants);
 
+//routs
 app.get('/', (req, res) => res.send('This is root!'));
-
+app.get('/plants', plantController.getAllPlants);//get all plants
+app.get('/plants/:id', plantController.getOnePlant);//get a spesific plant
+app.post('/plants', plantController.createPlant);//create a plant
+app.put('/plants/:id', plantController.updatePlant);//update a plant
+app.delete('/plants/:id', plantController.deletePlant);//create new plant
